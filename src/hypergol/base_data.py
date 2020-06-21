d666b5a3ebe40021a4f8448fd4beef5bf12ec396
Laszlo Sragner <sragner@gmail.com>
2020-06-21 15:02:37 +0100

    test split according to PR comments

--- src/hypergol/base_data.py
+++ src/hypergol/base_data.py
@@ -1,10 +1,12 @@
 import json
 
+MAX_MEMBER_REPR_LENGTH = 100
+
 
 class BaseData:
 
     def __repr__(self):
-        members = ', '.join(f'{k}={str(v)[:100]}' for k, v in self.__dict__.items())
+        members = ', '.join(f'{k}={str(v)[:MAX_MEMBER_REPR_LENGTH]}' for k, v in self.__dict__.items())
         return f"{self.__class__.__name__}({members})"
 
     def __str__(self):
@@ -31,19 +33,30 @@ class BaseData:
     def from_data(cls, data):
         return cls(**data)
 
-    def test(self):
+    def test_to_data(self):
+        originalData = self.__dict__.copy()
+        data = self.to_data()
+        for k, v in self.__dict__.items():
+            if v != originalData[k]:
+                raise AssertionError(f'{self.__class__.__name__}.to_data() changes the instance itself: {k}: {v} != {originalData[k]}')
         try:
-            originalData = self.__dict__.copy()
-            data = self.to_data()
-            for k, v in self.__dict__.items():
-                if v != originalData[k]:
-                    raise AssertionError(f'{self.__class__.__name__}.to_data() changes itself: {k}: {v} != {originalData[k]}')
-            selfCopy = self.from_data(json.loads(json.dumps(data)))
-            for k, v in selfCopy.__dict__.items():
-                if v != self.__dict__[k]:
-                    raise AssertionError(f'{self.__class__.__name__}.from_data() does not deserialise: {k}: {v} != {self.__dict__[k]}')
-            if not isinstance(self, type(selfCopy)):
-                raise AssertionError(f'{self.__class__.__name__}.from_data() does not return the correct type: {self.__class__.__name__} vs {selfCopy.__class__.__name__}')
-            return True
+            _ = json.dumps(data)
         except TypeError as ex:
-            raise TypeError(f'{self.__class__.__name__} self test failed: {ex}')
+            raise TypeError(f'{self.__class__.__name__} JSON serde test failed: {ex}')
+        return True
+
+    def test_from_data(self):
+        selfCopy = self.from_data(json.loads(json.dumps(self.to_data())))
+        if not isinstance(self, type(selfCopy)):
+            raise AssertionError(f'{self.__class__.__name__}.from_data() does not return the correct type: {self.__class__.__name__} vs {selfCopy.__class__.__name__}, from_data() return value should be "cls(**data)"')
+        for k, v in selfCopy.__dict__.items():
+            if v != self.__dict__[k]:
+                if str(k) == str(v):
+                    raise AssertionError(f'{self.__class__.__name__}.from_data() returns keys as values: {k}: {v} != {self.__dict__[k]}, from_data() return value should be "cls(**data)"')
+                raise AssertionError(f'{self.__class__.__name__}.from_data() does not deserialise: {k}: {v} != {self.__dict__[k]}')
+        return True
+
+    def test_serialisation(self):
+        self.test_to_data()
+        self.test_from_data()
+        return True
