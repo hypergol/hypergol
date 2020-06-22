106759b3a6befd0a58b69ad531b1dd7a21617cff
PattenR <rhyspatten@googlemail.com>
2020-06-22 20:19:38 +0100

    .

--- run_project_tests.sh
+++ run_project_tests.sh
@@ -2,4 +2,4 @@
 python src/hypergol.py create_project alpha && \
 python src/hypergol.py create_datamodel alpha article articleId:int url:str title:str text:str && \
 python src/hypergol.py create_datamodel alpha sentence startChar:int endChar:int articleId:int sentenceId:int && \
-diff alpha/ src/test_projects/alpha
+diff --suppress-common-lines --no-ignore-file-name-case --recursive alpha/ src/test_projects/alpha
