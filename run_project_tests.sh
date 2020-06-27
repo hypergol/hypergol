a3075c9801016a84d41c3873542beba450eb4df7
Laszlo Sragner <sragner@gmail.com>
2020-06-27 18:00:15 +0100

    run_project_tests.sh updated

--- run_project_tests.sh
+++ run_project_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+cd src
 pip3 install -e .
+cd ..
 python src/hypergol/cli/create_project.py alpha && \
 python src/hypergol/cli/create_data_model.py Article articleId:int url:str title:str text:str --projectDirectory=alpha && \
 python src/hypergol/cli/create_data_model.py Sentence startChar:int endChar:int articleId:int sentenceId:int --projectDirectory=alpha && \
