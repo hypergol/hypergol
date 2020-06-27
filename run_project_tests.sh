74d4c818d29d19049cd164b85f50797615d0f624
Laszlo Sragner <sragner@gmail.com>
2020-06-27 22:15:43 +0100

    fixing the second example

--- run_project_tests.sh
+++ run_project_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# cd src
-# pip3 install -e .
-# cd ..
-# python src/hypergol/cli/create_project.py alpha && \
-# python src/hypergol/cli/create_data_model.py Article articleId:int url:str title:str text:str --projectDirectory=alpha && \
-# python src/hypergol/cli/create_data_model.py Sentence startChar:int endChar:int articleId:int sentenceId:int --projectDirectory=alpha && \
-# diff --suppress-common-lines --no-ignore-file-name-case --recursive alpha/ src/test_projects/alpha/
+cd src
+pip3 install -e .
+cd ..
+python src/hypergol/cli/create_project.py alpha && \
+python src/hypergol/cli/create_data_model.py Article articleId:int url:str title:str text:str --projectDirectory=alpha && \
+python src/hypergol/cli/create_data_model.py Sentence startChar:int endChar:int articleId:int sentenceId:int --projectDirectory=alpha && \
+diff --suppress-common-lines --no-ignore-file-name-case --recursive alpha/ src/test_projects/alpha/
 
 python src/hypergol/cli/create_project.py example && \
 python src/hypergol/cli/create_data_model.py --projectDirectory=example Token i:int startChar:int endChar:int depType:str depHead:int depLeftEdge:int depRightEdge:int posType:str posFineType:str lemma:str text:str && \
