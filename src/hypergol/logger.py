233fd8136cc5b7a20e8ca85efb5de4f17677f838
Laszlo Sragner <sragner@gmail.com>
2020-07-07 14:31:26 +0100

    logger

---
+++ src/hypergol/logger.py
@@ -0,0 +1,41 @@
+import logging
+
+
+class Logger:
+
+    INFO = logging.INFO
+    DEBUG = logging.DEBUG
+
+    def __init__(self, path=None, level=logging.INFO, overWrite=False):
+        self.path = str(path)
+        self.level = level
+        self.overWrite = overWrite
+
+        self._logger = logging.getLogger()
+        self._logger.setLevel(level=self.level)
+        handlers = self._logger.handlers.copy()
+        for handler in handlers:
+            self._logger.removeHandler(handler)
+
+        handler=logging.StreamHandler(stream=None)
+        handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+        handler.setLevel(level=self.level)
+        self._logger.addHandler(handler)
+        if self.path is not None:
+            handler=logging.FileHandler(self.path, mode='w' if self.overWrite else 'a')
+            handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+            handler.setLevel(level=self.level)
+            self._logger.addHandler(handler)
+
+
+    def __reduce_ex__(self, protocol):
+        return self.__class__, (self.path, self.level, self.overWrite)
+
+    def exception(self, message):
+        self._logger.exception(message)
+
+    def info(self, message):
+        self._logger.info(message)
+
+    def log(self, message):
+        self.info(message)
---
+++ src/tests/test_logger.py
@@ -0,0 +1,42 @@
+import os
+import pickle
+from unittest import TestCase
+from hypergol import Logger
+
+
+class TestLogger(TestCase):
+
+    def __init__(self, methodName):
+        super(TestLogger, self).__init__(methodName=methodName)
+        self.logFile = 'testLogFile.tmp'
+
+    def tearDown(self):
+        super().tearDown()
+        if os.path.exists(self.logFile):
+            os.remove(self.logFile)
+
+    def test_pickle_logger(self):
+        logger = Logger(path=self.logFile)
+        newLogger = pickle.loads(pickle.dumps(logger))
+        self.assertEqual(logger.__dict__, newLogger.__dict__)
+
+    def test_logger_logs_into_files(self):
+        firstMessage = 'firstMessage'
+        secondMessage = 'secondMessage'
+        with open(self.logFile, 'wt') as f:
+            f.write(f'{firstMessage}\n')
+        logger = Logger(path=self.logFile)
+        logger.info(secondMessage)
+        results = open(self.logFile, 'rt').read().split('\n')
+        self.assertEqual(results[0], firstMessage)
+        self.assertEqual(results[1].endswith(secondMessage), True)
+
+    def test_logger_overwrites_files(self):
+        firstMessage = 'firstMessage'
+        secondMessage = 'secondMessage'
+        with open(self.logFile, 'wt') as f:
+            f.write(f'{firstMessage}\n')
+        logger = Logger(path=self.logFile, overWrite=True)
+        logger.info(secondMessage)
+        results = open(self.logFile, 'rt').read().split('\n')
+        self.assertEqual(results[0].endswith(secondMessage), True)
