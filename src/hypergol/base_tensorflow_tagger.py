9e030b8d9ee835dba5260c06fc38b75bee80305f
Mike Doran <mikrdoran@gmail.com>
2020-08-03 11:14:43 +0100

    added exception message

--- src/hypergol/base_tensorflow_tagger.py
+++ src/hypergol/base_tensorflow_tagger.py
@@ -4,16 +4,12 @@ import tensorflow as tf
 class BaseTensorflowTagger:
 
     def __init__(self, modelDirectory, useGPU, threads=None):
-        self.modelDirectory = modelDirectory
-        self.useGPU = useGPU
-        if not self.useGPU:
+        if not useGPU:
             tf.config.experimental.set_visible_devices([], 'GPU')
-        self.threads = threads
         if threads is not None:
-            tf.config.threading.set_inter_op_parallelism_threads(self.threads)
-            tf.config.threading.set_intra_op_parallelism_threads(self.threads)
+            tf.config.threading.set_inter_op_parallelism_threads(threads)
+            tf.config.threading.set_intra_op_parallelism_threads(threads)
         self.model = tf.saved_model.load(export_dir=f'{modelDirectory}/')
 
     def get_prediction(self, **kwargs):
-        """ Should call the `get_outputs` function of the model, for consistent interface with TF serve """
-        raise NotImplementedError
+        raise NotImplementedError(f'{self.__class__.__name__} must implement `get_prediction` function')
