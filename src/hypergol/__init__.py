dbcff241e18a10e8464234261cabfe68f5ef609d
Laszlo Sragner <sragner@gmail.com>
2020-08-07 22:09:10 +0100

    CLI speedup hack by not loading TF

--- src/hypergol/__init__.py
+++ src/hypergol/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from .pipeline import Pipeline
 from .base_data import BaseData
 from .delayed import Delayed
@@ -9,8 +10,10 @@ from .source import Source
 from .task import Task
 from .utils import Repr
 from .logger import Logger
-from .tensorflow_model_manager import TensorflowModelManager
-from .tensorflow_tagger import TensorflowTagger
-from .base_batch_processor import BaseBatchProcessor
-from .base_tensorflow_model_block import BaseTensorflowModelBlock
-from .base_tensorflow_model import BaseTensorflowModel
+# This spicy hack allows not to load TF each time we run CLI command which of course doesn't use TF - Laszlo
+if '-m' not in sys.argv:
+    from .tensorflow_model_manager import TensorflowModelManager
+    from .tensorflow_tagger import TensorflowTagger
+    from .base_batch_processor import BaseBatchProcessor
+    from .base_tensorflow_model_block import BaseTensorflowModelBlock
+    from .base_tensorflow_model import BaseTensorflowModel
