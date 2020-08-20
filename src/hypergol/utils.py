9ee0b063f342d2581091cc2d1bfd7eccf1c2080f
Laszlo Sragner <sragner@gmail.com>
2020-08-20 12:12:03 +0100

    utils shouldn't load TF or CLI is slow

--- src/hypergol/utils.py
+++ src/hypergol/utils.py
@@ -2,18 +2,8 @@ import os
 import hashlib
 import inspect
 from typing import List
-import tensorflow as tf
 from pydantic import create_model
 from hypergol.base_data import BaseData
-
-
-def load_model(modelDirectory, threads, useGPU):
-    if not useGPU:
-        tf.config.experimental.set_visible_devices([], 'GPU')
-    if threads is not None:
-        tf.config.threading.set_inter_op_parallelism_threads(threads)
-        tf.config.threading.set_intra_op_parallelism_threads(threads)
-    return tf.saved_model.load(export_dir=modelDirectory)
 
 
 def create_pydantic_type(type_):
