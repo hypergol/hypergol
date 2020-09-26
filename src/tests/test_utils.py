9727cc9c596b634972d99de5da8a824b1a08dbcc
xLaszlo <sragner@gmail.com>
2020-09-26 13:37:14 +0100

    Check if reqs and setup.py is the same (#66)
    * check if reqs and setup.py is the same
    
    * downgrade numpy to 1.8.5 to work with TF

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,1 +1,17 @@
 # TODO(Laszlo): test "Mode related file operations here"
+
+from unittest import TestCase
+
+class RequirementsText(TestCase):
+
+    def test_requirements(self):
+        import os
+        print(os.getcwd())
+        requirements = list(open('src/requirements.txt'))
+        for line in open('src/setup.py'):
+            if 'install_requires' in line:
+                line = line[line.index('[')+1:line.index(']')].replace("'", '')
+                installRequires = [req.strip() for req in line.split(',')]
+        self.assertEqual(len(requirements), len(installRequires))
+        for req, installReq in zip(requirements, installRequires):
+            self.assertTrue(req.startswith(installReq), msg=f'{req} and {installReq} does not match')
