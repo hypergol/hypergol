4588b1091953b27bda3c7358a2bbd80eb0a5aeff
Laszlo Sragner <sragner@gmail.com>
2020-11-29 17:24:46 +0000

    fixing pipeline test

--- src/tests/cli/test_create_pipeline.py
+++ src/tests/cli/test_create_pipeline.py
@@ -76,8 +76,6 @@ class TestCreatePipeline(HypergolCreateTestCase):
         super().setUp()
         self.project = HypergolProject(
             projectDirectory=self.projectDirectory,
-            dryrun=True,
-            force=None,
             repoManager=TestRepoManager(raiseIfDirty=False)
         )
         self.project.create_project_directory()
@@ -92,6 +90,6 @@ class TestCreatePipeline(HypergolCreateTestCase):
     @mock.patch('hypergol.cli.create_pipeline.HypergolProject.is_data_model_class', side_effect=lambda x: x.asClass in ['DataModelTestClass'])
     @mock.patch('hypergol.cli.create_pipeline.HypergolProject.is_task_class', side_effect=lambda x: x.asClass in ['OtherTask', 'ExampleSource'])
     def test_create_pipeline_creates_content(self, mock_is_task_class, mock_is_data_model_class, mock_check_dependencies):
-        content, scriptContent = create_pipeline('TestPipeline', 'DataModelTestClass', 'ExampleSource', 'OtherTask', projectDirectory='test_project', dryrun=True, project=self.project)
+        content, scriptContent = create_pipeline('TestPipeline', 'DataModelTestClass', 'ExampleSource', 'OtherTask', projectDirectory='test_project', dryrun=True)
         self.assertEqual(content, TEST_CONTENT)
         self.assertEqual(scriptContent, TEST_SHELL)
