0426e555b7a7b2e106a4430b03d0ffedd066a9e8
xLaszlo <sragner@gmail.com>
2022-02-19 18:44:28 +0100

    Bump numpy to 1.21 (#112)

--- src/setup.py
+++ src/setup.py
@@ -23,6 +23,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['fire', 'Jinja2', 'GitPython', 'numpy==1.19.5', 'tensorflow==2.5.3', 'tqdm', 'pydantic', 'fastapi', 'uvicorn'],
+    install_requires=['fire', 'Jinja2', 'GitPython', 'numpy==1.21', 'tensorflow==2.5.3', 'tqdm', 'pydantic', 'fastapi', 'uvicorn'],
     include_package_data=True
 )
