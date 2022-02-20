53498069e31ceea39359e8d1fb2e99a661f31049
Laszlo Sragner <sragner@gmail.com>
2022-02-20 18:15:09 +0000

    __init__ exports new classes

--- src/hypergol/__init__.py
+++ src/hypergol/__init__.py
@@ -17,10 +17,15 @@ from .hypergol_project import RepoManager
 from .hypergol_project import HypergolProject
 # This spicy hack allows not to load TF each time we run CLI command which of course doesn't use TF - Laszlo
 if '-m' not in sys.argv:
-    from .tensorflow_model_manager import TensorflowModelManager
     from .base_batch_processor import BaseBatchProcessor
+
+    from .tensorflow_model_manager import TensorflowModelManager
     from .base_tensorflow_model_block import BaseTensorflowModelBlock
     from .base_tensorflow_model import BaseTensorflowModel
+
+    from .torch_model_manager import TorchModelManager
+    from .base_torch_model_block import BaseTorchModelBlock
+    from .base_torch_model import BaseTorchModel
 
 try:
     __version__ = get_distribution('hypergol').version
