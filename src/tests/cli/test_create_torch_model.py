d58c8f85380fc07b0ab62b27a34ae1cd18d37ef7
Laszlo Sragner <sragner@gmail.com>
2022-03-01 21:41:01 +0000

    fix torch batchprocessor

--- src/tests/cli/test_create_torch_model.py
+++ src/tests/cli/test_create_torch_model.py
@@ -43,17 +43,18 @@ class TestTorchModel(BaseTorchModel):
 """.lstrip()
 
 TEST_BATCH_PROCESSOR = """
-import tensorflow as tf
+import torch
+import torch.nn as nn
 from hypergol import BaseBatchProcessor
 
 from data_models.test_evaluation_class import TestEvaluationClass
 from data_models.test_output import TestOutput
 
 
-class TestModelBatchProcessor(BaseBatchProcessor):
+class TestTorchModelBatchProcessor(BaseBatchProcessor):
 
     def __init__(self, inputDataset, inputBatchSize, outputDataset, exampleArgument):
-        super(TestModelBatchProcessor, self).__init__(inputDataset, inputBatchSize, outputDataset)
+        super(TestTorchModelBatchProcessor, self).__init__(inputDataset, inputBatchSize, outputDataset)
         self.exampleArgument = exampleArgument
 
     def process_input_batch(self, batch):
@@ -66,16 +67,17 @@ class TestModelBatchProcessor(BaseBatchProcessor):
         #     exampleInput1.append(exampleValue.exampleList)
         #     exampleInput2.append(len(exampleValue.exampleList))
         # inputs = {
-        #     'exampleInput1': tf.ragged.constant(lemmas, dtype=tf.string).to_tensor()[:, 10]
-        #     'exampleInput2': tf.constant(sentenceLengths, dtype=tf.int32)
+        #     'exampleInput1': torch.IntTensor(exampleInput1),
+        #     'exampleInput2': torch.FloatTensor(exampleInput2),
         # }
         # logic can be combined with process_training_batch
+        # all return values must be torch Tensors!!!! including ids!!!
         return inputs
 
     def process_training_batch(self, batch):
         raise NotImplementedError('BaseBatchProcessor must implement process_training_batch()')
         # batch is a list of datamodel objects cycle through them and build the data
-        # that can be converted to tensorflow constants, e.g.:
+        # that can be converted to torch tensors, e.g.:
         # exampleInput1 = []
         # exampleInput2 = []
         # exampleOutput = []
@@ -84,10 +86,10 @@ class TestModelBatchProcessor(BaseBatchProcessor):
         #     exampleInput2.append(len(exampleValue.exampleList))
         #     exampleOutput.append(exampleValue.exampleOutputList)
         # inputs = {
-        #     'exampleInput1': tf.ragged.constant(lemmas, dtype=tf.string).to_tensor()[:, 10]
-        #     'exampleInput2': tf.constant(sentenceLengths, dtype=tf.int32)
+        #     'exampleInput1': torch.IntTensor(exampleInput1),
+        #     'exampleInput2': torch.FloatTensor(exampleInput2),
         # }
-        # targets = tf.ragged.constant(, dtype=tf.string).to_tensor()[:, :10]
+        # targets = torch.FloatTensor(...)
         return inputs, targets
 
     def process_output_batch(self, outputs):
@@ -104,6 +106,7 @@ class TestModelBatchProcessor(BaseBatchProcessor):
         # evaluationBatch = []
         # for id_, i, t, o in zip(inputs['ids'], inputs, targets, outputs):
         #     evaluationBatch.append(ExampleOutput(eoid=id_, i=i, t=t, o=o))
+        # BaseModel hash_id values must be converted to python values with id.item()!!!!
         # return evaluationBatch
 """.lstrip()
 
